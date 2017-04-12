56c655d99f05be32a34c8b60d916e67211dd18cf
Kevin Leung <leung.kevin23@gmail.com>
2017-04-12 14:27:53 -0500

    added dotenv package - setup bot connectors

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-// Bot Builder without LUIS
+var restify = require('restify')
+var builder = require('botbuilder')
+
+// =========================================================
+// Bot Setup
+// =========================================================
+
+// Setup Restify Server
+var server = restify.createServer()
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+  console.log('%s listening to %s', server.name, server.url)
+})
+
+// Create chat bot
+var connector = new builder.ChatConnector({
+  appId: process.env.APP_ID,
+  appPassword: process.env.APP_PASS
+})
+
+var bot = new builder.UniversalBot(connector)
+server.post('/api/messages', connector.listen())
+
+// =========================================================
+// Bots Dialogs
+// =========================================================
+bot.dialog('/')
--- appLUIS.js
+++ appLUIS.js
@@ -1,1 +1,32 @@
-// Bot Builder with LUIS
+require('dotenv').config()
+var restify = require('restify')
+var builder = require('botbuilder')
+
+// =========================================================
+// Bot Setup
+// =========================================================
+
+// Setup Restify Server
+var server = restify.createServer()
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+  console.log('%s listening to %s', server.name, server.url)
+})
+
+// Create chat bot
+var connector = new builder.ChatConnector({
+  appId: process.env.APP_ID,
+  appPassword: process.env.APP_PASS
+})
+
+var bot = new builder.UniversalBot(connector)
+server.post('/api/messages', connector.listen())
+
+// Setup LUIS connection
+var model = 'https://api.projectoxford.ai/luis/v1/application?id=' + process.env.LUIS_ID + '&subscription-key=' + process.env.LUIS_KEY + '&verbose=true'
+var recognizer = new builder.LuisRecognizer(model)
+var dialog = new builder.IntentDialog({recognizers: [recognizer]})
+bot.dialog('/', dialog)
+
+// =========================================================
+// Bots Dialogs
+// =========================================================
